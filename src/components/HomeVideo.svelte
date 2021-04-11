c283bd3878462f4491dac344d7be3ba1e0ba43db
Venkateshwaran Selvaraj <venkateshwarans@ilimi.in>
2021-04-11 17:51:18 +0530

    Adding slide on homepage

--- src/components/HomeVideo.svelte
+++ src/components/HomeVideo.svelte
@@ -1,8 +1,20 @@
+<script>
+import Carousel from './UI/Carousel.svelte';
+
+let videos = [{
+  path: 'https://player.vimeo.com/video/480711691?autoplay=0&title=0&byline=0&portrait=0',
+  id: 'image1'
+},{
+  path: 'https://player.vimeo.com/video/404927617?autoplay=0&title=0&byline=0&portrait=0',
+  id: 'image2'
+},{
+  path: 'https://player.vimeo.com/video/345425681?autoplay=0&title=0&byline=0&portrait=0',
+  id: 'image3'
+}]
+
+</script>
+
 <section class="mb-16 sm:mb-24 xs:mb-20 md:mb-32 lg:mb-40">
-  <div id="responsiveVideoWrapper" class="relative h-0 pb-fluid-video">
-    <iframe class="home-video absolute top-0 left-0 w-full h-full"
-      src="https://player.vimeo.com/video/345425681?autoplay=1&title=0&byline=0&portrait=0&background=1&muted=0&controls=1" frameborder="0"
-      allow="autoplay; fullscreen" allowfullscreen title="Flying Elephants Films Video">
-    </iframe>
-  </div>
+    <Carousel images={videos} iframe="true" controlColor="#fff"></Carousel>
 </section>
+
--- src/components/UI/Carousel.svelte
+++ src/components/UI/Carousel.svelte
@@ -1,12 +1,15 @@
 
 <script>
   import { flip } from 'svelte/animate';
+  import { fade, fly } from 'svelte/transition';
   import { onDestroy } from 'svelte';
   export let images;
+  export let iframe;
   export let imageWidth = '100%';
-  export let imageSpacing = 20;
+  export let imageSpacing = 0;
   export let speed = 500;
   export let controlColor= '#444';
+  export let strokeColor= '#000';
   export let controlScale = '0.5';
   export let autoplay = false;
   export let autoplaySpeed = 5000;
@@ -37,49 +40,93 @@
   }
   onDestroy(()=>{stopAutoPlay()})
 </script>
+<!-- width: 100vw -->
+{#if iframe=== 'true'}
+  <div id="carousel-container" class="carousel-container w-full relative flex flex-col overflow-x-hidden">
+    <div id="carousel-images-no-flex" class="w-full mx-auto justify-center h-0 pb-fluid-video overflow-y-hidden">
+    <!-- <div id="carousel-images"> -->
+        {#each images as image (image.id)}
+          <div id="responsiveVideoWrapper" class="w-full relative pb-fluid-video" in:fly="{{ x: 200, duration: 200 }}">
+            <iframe class="home-video absolute top-0 left-0 w-full h-full object-contain md:object-scale-down"
+            src={image.path}
+            style={`width:${imageWidth}; margin: 0 ${imageSpacing}px;`}
+            id={image.id}
+            on:mouseover={stopAutoPlay}
+            on:mouseout={startAutoPlay}
+            frameborder="0"
+            allow="autoplay; fullscreen" allowfullscreen title="Flying Elephants Films Video">
+            </iframe>
+          </div>
+        {/each}
 
-<div id="carousel-container" class="carousel-container w-full relative flex flex-col overflow-x-hidden">
-  <div id="carousel-images" class="w-full md:w-1/2 mx-auto flex justify-center flex-no-wrap max-h-screen">
-    {#each images as image (image.id)}
-      <img
-        class="object-contain md:object-scale-down"
-        src={image.path}
-        alt={image.id}
-        id={image.id}
-        style={`width:${imageWidth}; margin: 0 ${imageSpacing}px;`}
-        on:mouseover={stopAutoPlay}
-        on:mouseout={startAutoPlay}
-        animate:flip={{duration: speed}}/>
-    {/each}
+    </div>
+    {#if displayControls}
+      <button id="left" on:click={rotateLeft} class="">
+        <slot name="left-control">
+          <svg width="39px" height="110px" id="svg8" class="transform scale-50" transform={`scale(${controlScale})`}>
+            <g id="layer1" transform="translate(-65.605611,-95.36949)">
+              <path
+              style={`fill:none;stroke:${controlColor};stroke-width:9.865;stroke-linecap:round;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1`}
+              d="m 99.785711,100.30199 -23.346628,37.07648 c -7.853858,12.81098 -7.88205,12.81098 0,24.78902 l 23.346628,37.94647"
+              id="path1412" />
+            </g>
+          </svg>
+        </slot>
+      </button>
+      <button id="right" on:click={rotateRight} class="absolute top-1/2 -translate-y-1/2 flex items-center justify-center bg-transparent border-0">
+        <slot name="right-control">
+          <svg width="39px" height="110px" id="svg8" class="transform rotate-180 scale-50" transform={`rotate(180) scale(${controlScale})`}>
+            <g id="layer1" transform="translate(-65.605611,-95.36949)">
+              <path
+              style={`fill:none;stroke:${controlColor};stroke-width:9.865;stroke-linecap:round;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1`}
+              d="m 99.785711,100.30199 -23.346628,37.07648 c -7.853858,12.81098 -7.88205,12.81098 0,24.78902 l 23.346628,37.94647"
+              id="path1412" />
+            </g>
+          </svg>
+        </slot>
+      </button>
+    {/if}
   </div>
-  {#if displayControls}
-  <button id="left" on:click={rotateLeft} class="">
-    <slot name="left-control">
-      <svg width="39px" height="110px" id="svg8" class="transform scale-50" transform={`scale(${controlScale})`}>
-        <g id="layer1" transform="translate(-65.605611,-95.36949)">
-          <path
-          style={`fill:none;stroke:${controlColor};stroke-width:9.865;stroke-linecap:round;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1`}
-          d="m 99.785711,100.30199 -23.346628,37.07648 c -7.853858,12.81098 -7.88205,12.81098 0,24.78902 l 23.346628,37.94647"
-          id="path1412" />
-        </g>
-      </svg>
-    </slot>
-  </button>
-  <button id="right" on:click={rotateRight} class="absolute top-1/2 -translate-y-1/2 flex items-center justify-center bg-transparent border-0">
-    <slot name="right-control">
-      <svg width="39px" height="110px" id="svg8" class="transform rotate-180 scale-50" transform={`rotate(180) scale(${controlScale})`}>
-        <g id="layer1" transform="translate(-65.605611,-95.36949)">
-          <path
-          style={`fill:none;stroke:${controlColor};stroke-width:9.865;stroke-linecap:round;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1`}
-          d="m 99.785711,100.30199 -23.346628,37.07648 c -7.853858,12.81098 -7.88205,12.81098 0,24.78902 l 23.346628,37.94647"
-          id="path1412" />
-        </g>
-      </svg>
-    </slot>
-  </button>
-  {/if}
-</div>
-
-<style>
-
-</style>
+{:else}
+  <div id="carousel-container" class="carousel-container w-full relative flex flex-col overflow-x-hidden">
+    <div id="carousel-images" class="w-full md:w-1/2 mx-auto flex justify-center flex-no-wrap max-h-screen">
+      {#each images as image (image.id)}
+        <img
+          class="object-contain md:object-scale-down"
+          src={image.path}
+          alt={image.id}
+          id={image.id}
+          style={`width:${imageWidth}; margin: 0 ${imageSpacing}px;`}
+          on:mouseover={stopAutoPlay}
+          on:mouseout={startAutoPlay}
+          animate:flip={{duration: speed}}/>
+        {/each}
+    </div>
+    {#if displayControls}
+      <button id="left" on:click={rotateLeft} class="">
+        <slot name="left-control">
+          <svg width="39px" height="110px" id="svg8" class="transform scale-50" transform={`scale(${controlScale})`}>
+            <g id="layer1" transform="translate(-65.605611,-95.36949)">
+              <path
+              style={`fill:none;stroke:${controlColor};stroke-width:9.865;stroke-linecap:round;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1`}
+              d="m 99.785711,100.30199 -23.346628,37.07648 c -7.853858,12.81098 -7.88205,12.81098 0,24.78902 l 23.346628,37.94647"
+              id="path1412" />
+            </g>
+          </svg>
+        </slot>
+      </button>
+      <button id="right" on:click={rotateRight} class="absolute top-1/2 -translate-y-1/2 flex items-center justify-center bg-transparent border-0">
+        <slot name="right-control">
+          <svg width="39px" height="110px" id="svg8" class="transform rotate-180 scale-50" transform={`rotate(180) scale(${controlScale})`}>
+            <g id="layer1" transform="translate(-65.605611,-95.36949)">
+              <path
+              style={`fill:none;stroke:${controlColor};stroke-width:9.865;stroke-linecap:round;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1`}
+              d="m 99.785711,100.30199 -23.346628,37.07648 c -7.853858,12.81098 -7.88205,12.81098 0,24.78902 l 23.346628,37.94647"
+              id="path1412" />
+            </g>
+          </svg>
+        </slot>
+      </button>
+    {/if}
+  </div>
+{/if}
