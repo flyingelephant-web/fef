105cee0523af25e390bfca3f9c678797d2808453
Venkateshwaran <venkateshwaran.s@sarasanalytics.com>
2022-03-15 22:41:32 +0530

    new film added and address changed

---
+++ utils/data/change-15-03-2022.js
@@ -0,0 +1,172 @@
+module.exports = [
+  {
+    "clip": {
+        "uri": "/videos/686999178",
+        "name": "Jacob's Creek - Mithali Raj",
+        "description": "Proud to have worked on this inspiring tale of Mithali Raj for Jacob's Creek India\n\nBrand: Jacob's Creek\nAgency: Wavemaker India\nProduction House: Flying Elephant Films \nProducers: Sanjana Agarwal, Smriti Sharma Bhaskar \nAnimation & VFX: Gagandeep Kochar\nVoice Over- Jasleen",
+        "type": "video",
+        "duration": 30,
+        "width": 1080,
+        "height": 1080,
+        "created_time": "2022-03-11T06:28:03+00:00",
+        "content_rating": [
+            "unrated"
+        ],
+        "config_url": "https://player.vimeo.com/video/686999178/config?autopause=0&autoplay=0&background=0&badge=0&byline=0&bypass_privacy=1&collections=0&context=Vimeo%5CController%5CApi%5CResources%5CUser%5CProfile%5CProfileSectionVideosController.&controls=1&default_to_hd=1&force_embed=0&h=1882e0d370&like=1&logo=0&loop=0&muted=0&offset=1&outro=nothing&playbar=1&portrait=0&privacy_banner=0&quality=540p&responsive=1&responsive_width=1&share=1&speed=1&title=0&volume=1&watch_later=1&s=932a714bfd5761e90a0a7f3109036468b7d0c4ab_1647463841",
+        "pictures": {
+            "uri": "/videos/686999178/pictures/1391796621",
+            "active": true,
+            "type": "custom",
+            "base_link": "https://i.vimeocdn.com/video/1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d",
+            "sizes": [
+                {
+                    "width": 100,
+                    "height": 75,
+                    "link": "https://i.vimeocdn.com/video/1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_100x75?r=pad",
+                    "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_100x75&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+                },
+                {
+                    "width": 200,
+                    "height": 150,
+                    "link": "https://i.vimeocdn.com/video/1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_200x150?r=pad",
+                    "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_200x150&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+                },
+                {
+                    "width": 295,
+                    "height": 166,
+                    "link": "https://i.vimeocdn.com/video/1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_295x166?r=pad",
+                    "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_295x166&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+                },
+                {
+                    "width": 640,
+                    "height": 360,
+                    "link": "https://i.vimeocdn.com/video/1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_640x360?r=pad",
+                    "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_640x360&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+                },
+                {
+                    "width": 960,
+                    "height": 540,
+                    "link": "https://i.vimeocdn.com/video/1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_960x540?r=pad",
+                    "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_960x540&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+                },
+                {
+                    "width": 1280,
+                    "height": 720,
+                    "link": "https://i.vimeocdn.com/video/1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_1280x720?r=pad",
+                    "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_1280x720&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+                },
+                {
+                    "width": 1920,
+                    "height": 1080,
+                    "link": "https://i.vimeocdn.com/video/1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_1920x1080?r=pad",
+                    "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_1920x1080&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+                }
+            ],
+            "resource_key": "df55cd196b957ff6b2716059c4302c9fe55d9106",
+            "default_picture": false
+        },
+        "categories": [],
+        "metadata": {
+            "connections": {
+                "comments": {
+                    "total": 0
+                }
+            }
+        },
+        "user": {
+            "uri": "/users/49086211",
+            "name": "Flying Elephant Films",
+            "link": "https://vimeo.com/flyingelephantfilms",
+            "location": "New Delhi, Delhi, India",
+            "bio": "We are a one stop shop for all things film. From conceptualising to execution, we bring our unique perspective, experience and skill set to every project. Always striving to challenge the ordinary and experimenting to break the norm, we want our every product to make us proud. An award winning team that brings its experience of film making in all formats.",
+            "membership": {
+                "badge": {
+                    "type": "pro",
+                    "text": "PRO",
+                    "alt_text": "Learn more about Vimeo PRO",
+                    "url": "/pro"
+                }
+            },
+            "pictures": {
+                "sizes": [
+                    {
+                        "width": 30,
+                        "height": 30,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_30x30"
+                    },
+                    {
+                        "width": 72,
+                        "height": 72,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_72x72"
+                    },
+                    {
+                        "width": 75,
+                        "height": 75,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_75x75"
+                    },
+                    {
+                        "width": 100,
+                        "height": 100,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_100x100"
+                    },
+                    {
+                        "width": 144,
+                        "height": 144,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_144x144"
+                    },
+                    {
+                        "width": 216,
+                        "height": 216,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_216x216"
+                    },
+                    {
+                        "width": 288,
+                        "height": 288,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_288x288"
+                    },
+                    {
+                        "width": 300,
+                        "height": 300,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_300x300"
+                    },
+                    {
+                        "width": 360,
+                        "height": 360,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_360x360"
+                    }
+                ]
+            },
+            "skills": [],
+            "available_for_hire": true,
+            "background_video": [
+                {
+                    "id": 1563780821,
+                    "clip_id": "654920183",
+                    "y_position": 50,
+                    "start_time": 3,
+                    "end_time": 17,
+                    "clip": {},
+                    "first_frame_URL": "https://i.vimeocdn.com/profile_cover/1563780821",
+                    "page_url": "/654920183",
+                    "dimensions": {
+                        "width": 1920,
+                        "height": 1080
+                    },
+                    "config_url": "https://player.vimeo.com/video/654920183/config?autopause=0&autoplay=1&background=1&bypass_privacy=1&context=Vimeo%5CController%5CApi%5CResources%5CUser%5CProfile%5CProfileSectionVideosController.&default_to_hd=1&h=663c7b1ba3&log_plays=0&outro=nothing&responsive=1&responsive_width=1&s=c554dde63f3b4871a16040afb55c3c2c7ccf718e_1647463841",
+                    "player_url": "player.vimeo.com",
+                    "thumbset_id": 1321830641,
+                    "aspect_ratio": 0.562429696287964,
+                    "duration": 47
+                }
+            ],
+            "is_expert": false
+        },
+        "badge": null
+    },
+    "profile_section_uri": "/users/49086211/profile_sections/328639",
+    "show_featured_comment": false,
+    "featured_comment": null,
+    "is_staffpick": false,
+    "column_width": "1"
+}
+]
--- utils/data/changes.js
+++ utils/data/changes.js
@@ -1,42 +1,43 @@
-[{
-  "description": "Presenting another spot, yet again.\n\n\nYear after year, it gives us pure joy to keep working and making commercials for Lacto Calamine. Cheers to us, cheers to the entire team!",
+[
+  {
+  "description": "Proud to have worked on this inspiring tale of Mithali Raj for Jacob's Creek India\n\nBrand: Jacob's Creek\nAgency: Wavemaker India\nProduction House: Flying Elephant Films \nProducers: Sanjana Agarwal, Smriti Sharma Bhaskar \nAnimation & VFX: Gagandeep Kochar\nVoice Over- Jasleen",
   "duration": 30,
-  "name": "Lacto Calamine Amyra Dastur 30 Seconds 2022",
+  "name": "Jacob's Creek - Mithali Raj",
   "pictures": {
-    "uri": "/videos/680380258/pictures/1379125558",
+    "uri": "/videos/686999178/pictures/1391796621",
     "active": true,
     "type": "custom",
-    "base_link": "https://i.vimeocdn.com/video/1379125558-cbc95545885eadb549f774ce11f0490ab52d8ecdecc629408c13a943211daaed-d",
+    "base_link": "https://i.vimeocdn.com/video/1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d",
     "sizes": [{
       "width": 200,
       "height": 150,
-      "link": "https://i.vimeocdn.com/video/1379125558-cbc95545885eadb549f774ce11f0490ab52d8ecdecc629408c13a943211daaed-d_200x150?r=pad",
-      "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1379125558-cbc95545885eadb549f774ce11f0490ab52d8ecdecc629408c13a943211daaed-d_200x150&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+      "link": "https://i.vimeocdn.com/video/1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_200x150?r=pad",
+      "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_200x150&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
     }, {
       "width": 640,
       "height": 360,
-      "link": "https://i.vimeocdn.com/video/1379125558-cbc95545885eadb549f774ce11f0490ab52d8ecdecc629408c13a943211daaed-d_640x360?r=pad",
-      "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1379125558-cbc95545885eadb549f774ce11f0490ab52d8ecdecc629408c13a943211daaed-d_640x360&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+      "link": "https://i.vimeocdn.com/video/1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_640x360?r=pad",
+      "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_640x360&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
     }, {
       "width": 1280,
       "height": 720,
-      "link": "https://i.vimeocdn.com/video/1379125558-cbc95545885eadb549f774ce11f0490ab52d8ecdecc629408c13a943211daaed-d_1280x720?r=pad",
-      "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1379125558-cbc95545885eadb549f774ce11f0490ab52d8ecdecc629408c13a943211daaed-d_1280x720&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+      "link": "https://i.vimeocdn.com/video/1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_1280x720?r=pad",
+      "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_1280x720&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
     }, {
       "width": 1920,
       "height": 1080,
-      "link": "https://i.vimeocdn.com/video/1379125558-cbc95545885eadb549f774ce11f0490ab52d8ecdecc629408c13a943211daaed-d_1920x1080?r=pad",
-      "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1379125558-cbc95545885eadb549f774ce11f0490ab52d8ecdecc629408c13a943211daaed-d_1920x1080&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+      "link": "https://i.vimeocdn.com/video/1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_1920x1080?r=pad",
+      "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1391796621-bafdafbcd9ba594e35e39013e5d866c5e854ae5ebba4df4e0d4af11d818569dd-d_1920x1080&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
     }],
-    "resource_key": "dd2f64365fcb4b7cc2b7937a487b799943764051",
+    "resource_key": "df55cd196b957ff6b2716059c4302c9fe55d9106",
     "default_picture": false
   },
   "type": "",
-  "uri": "/videos/680380258",
+  "uri": "/videos/686999178",
   "user": {
     "name": "Flying Elephant Films"
   },
-  "slug": "lacto-calamine-amyra-dastur-30-seconds-2022",
+  "slug": "jacobs-creek-mithali-raj",
   "id": "",
   "title": "",
   "blurb": "",
@@ -74,4 +75,5 @@
     "website": "",
     "image": ""
   }]
-}]
+}
+]
