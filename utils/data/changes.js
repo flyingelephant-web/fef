81795e8b8f1239b3865aa0b216f392a8468a00c4
Venkateshwaran <venkateshwaran.s@sarasanalytics.com>
2022-07-04 18:50:33 +0530

    Revert "adding love depot digital film"
    This reverts commit 112447b3fa0d6d05328abda78ce55c5187b74e01.

--- utils/data/changes.js
+++ utils/data/changes.js
@@ -1,77 +1,77 @@
 [{
-    "description": "One of the most informative and educated campaign for us.\n\nNow we know that the right instruments can take out the right noises �\n\nClient: Love Depot\nAgency: The Glitch\nProduction House: Flying Elephant Films\nDirector: Aneesh Malankar\nDP: Jay Charola\nExecutive Producers: Sanjana Agarwal & Riddhindra R Chaudhuri \nCreative Supervisor: Smriti Sharma Bhaskar\nDA: Tamara Jose\nChief AD: Ragini Gunjan\n2nd AD: Ayan Poddar\n3rd AD: Shantanu Paliwal\nIntern: Chaitanya Jain\nProduction Designer: Parul Bose\nCasting Director: Kapil Khanna\nLine Production: Tafreeh Studios\nEditor: Abhishek Das\nOnline: Arun & Parag\nVO: Tanya Nambiar\nMusic - Aaj Mausam Bada Beimaan Hai | The Bartender Mix (2014) | Mauli Dave, Mikey McCleary, Saregama Karaoke and Music",
-    "duration": 69,
-    "name": "Love Depot DVC 2022 - Director's Cut",
-    "pictures": {
-        "uri": "/videos/726614591/pictures/1462842397",
-        "active": true,
-        "type": "custom",
-        "base_link": "https://i.vimeocdn.com/video/1462842397-8a11be1c5fd72e788620cf5c3a125961a522cee26074f3e8f2926cd12d99d170-d",
-        "sizes": [{
-            "width": 200,
-            "height": 150,
-            "link": "https://i.vimeocdn.com/video/1462842397-8a11be1c5fd72e788620cf5c3a125961a522cee26074f3e8f2926cd12d99d170-d_200x150?r=pad",
-            "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1462842397-8a11be1c5fd72e788620cf5c3a125961a522cee26074f3e8f2926cd12d99d170-d_200x150&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
-        }, {
-            "width": 640,
-            "height": 360,
-            "link": "https://i.vimeocdn.com/video/1462842397-8a11be1c5fd72e788620cf5c3a125961a522cee26074f3e8f2926cd12d99d170-d_640x360?r=pad",
-            "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1462842397-8a11be1c5fd72e788620cf5c3a125961a522cee26074f3e8f2926cd12d99d170-d_640x360&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
-        }, {
-            "width": 1280,
-            "height": 720,
-            "link": "https://i.vimeocdn.com/video/1462842397-8a11be1c5fd72e788620cf5c3a125961a522cee26074f3e8f2926cd12d99d170-d_1280x720?r=pad",
-            "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1462842397-8a11be1c5fd72e788620cf5c3a125961a522cee26074f3e8f2926cd12d99d170-d_1280x720&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
-        }, {
-            "width": 1920,
-            "height": 1080,
-            "link": "https://i.vimeocdn.com/video/1462842397-8a11be1c5fd72e788620cf5c3a125961a522cee26074f3e8f2926cd12d99d170-d_1920x1080?r=pad",
-            "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1462842397-8a11be1c5fd72e788620cf5c3a125961a522cee26074f3e8f2926cd12d99d170-d_1920x1080&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
-        }],
-        "resource_key": "4015982b35bdb9e9dba0482a3be68691faa9fe52",
-        "default_picture": false
-    },
-    "type": "",
-    "uri": "/videos/726614591",
-    "user": {
-        "name": "Flying Elephant Films"
-    },
-    "slug": "love-depot-dvc-2022-directors-cut",
+  "description": null,
+  "duration": 153,
+  "name": "PULSAR - BATTLING GRAVITY (DIRECTOR'S CUT)",
+  "pictures": {
+    "uri": "/videos/713221500/pictures/1437883617",
+    "active": true,
+    "type": "custom",
+    "base_link": "https://i.vimeocdn.com/video/1437883617-988f81ce6810ff60c84a9420772a555b62e2e8443891fa1214ef5be69bac171e-d",
+    "sizes": [{
+      "width": 200,
+      "height": 150,
+      "link": "https://i.vimeocdn.com/video/1437883617-988f81ce6810ff60c84a9420772a555b62e2e8443891fa1214ef5be69bac171e-d_200x150?r=pad",
+      "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1437883617-988f81ce6810ff60c84a9420772a555b62e2e8443891fa1214ef5be69bac171e-d_200x150&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+    }, {
+      "width": 640,
+      "height": 360,
+      "link": "https://i.vimeocdn.com/video/1437883617-988f81ce6810ff60c84a9420772a555b62e2e8443891fa1214ef5be69bac171e-d_640x360?r=pad",
+      "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1437883617-988f81ce6810ff60c84a9420772a555b62e2e8443891fa1214ef5be69bac171e-d_640x360&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+    }, {
+      "width": 1280,
+      "height": 720,
+      "link": "https://i.vimeocdn.com/video/1437883617-988f81ce6810ff60c84a9420772a555b62e2e8443891fa1214ef5be69bac171e-d_1280x720?r=pad",
+      "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1437883617-988f81ce6810ff60c84a9420772a555b62e2e8443891fa1214ef5be69bac171e-d_1280x720&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+    }, {
+      "width": 1920,
+      "height": 1080,
+      "link": "https://i.vimeocdn.com/video/1437883617-988f81ce6810ff60c84a9420772a555b62e2e8443891fa1214ef5be69bac171e-d_1920x1080?r=pad",
+      "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1437883617-988f81ce6810ff60c84a9420772a555b62e2e8443891fa1214ef5be69bac171e-d_1920x1080&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+    }],
+    "resource_key": "27f94183623a867ed16479d5601c90e24a93324b",
+    "default_picture": false
+  },
+  "type": "",
+  "uri": "/videos/713221500",
+  "user": {
+    "name": "Flying Elephant Films"
+  },
+  "slug": "pulsar-battling-gravity-directors-cut",
+  "id": "",
+  "title": "",
+  "blurb": "",
+  "content": "",
+  "category": [],
+  "tags": [],
+  "articleImage": "",
+  "thumbnailImage": "",
+  "index": "",
+  "author": [],
+  "media": [{
+    "id": "",
+    "mediaType": "",
+    "src": "",
+    "caption": ""
+  }],
+  "crew": [{
+    "id": "",
+    "name": "",
+    "photo": "",
+    "bio": "",
+    "role": ""
+  }],
+  "awards": [{
     "id": "",
     "title": "",
+    "year": "",
     "blurb": "",
-    "content": "",
-    "category": [],
-    "tags": [],
-    "articleImage": "",
-    "thumbnailImage": "",
-    "index": "",
-    "author": [],
-    "media": [{
-        "id": "",
-        "mediaType": "",
-        "src": "",
-        "caption": ""
-    }],
-    "crew": [{
-        "id": "",
-        "name": "",
-        "photo": "",
-        "bio": "",
-        "role": ""
-    }],
-    "awards": [{
-        "id": "",
-        "title": "",
-        "year": "",
-        "blurb": "",
-        "reference": "",
-        "image": ""
-    }],
-    "partners": [{
-        "id": "",
-        "name": "",
-        "website": "",
-        "image": ""
-    }]
+    "reference": "",
+    "image": ""
+  }],
+  "partners": [{
+    "id": "",
+    "name": "",
+    "website": "",
+    "image": ""
+  }]
 }]
