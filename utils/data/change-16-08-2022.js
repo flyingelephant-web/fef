cdfc387ff7fc903a7013a9a1360ef8e8f775a406
Venkateshwaran Selvaraj <venkateshwaran.selvaraj@gmail.com>
2022-08-16 13:24:29 +0530

    center fresh

---
+++ utils/data/change-16-08-2022.js
@@ -0,0 +1,172 @@
+module.exports = [{
+    "clip": {
+        "uri": "/videos/736453614",
+        "name": "Center Fresh - Fresh Breath Day",
+        "description": "The refreshing national crush of India - Rohit Saraf\nEveryone’s center of attention and queen - Alaya\nThe ‘love storiyan’ that everyone is hating but this no one can hate. Presenting our latest film for Center Fresh India\n\nBrand: Center Fresh India\nCreative Agency: Ogilvy India\nMedia Agency: Wave Maker India\n\nDirector: Sanana Jain\nExecutive Producer: Riddhindra Chaudhuri\nCreative Supervisor: Smriti Sharma Bhaskar\nDP: Sombit Mondal\nAC: Roshan Jose\nChief AD: Ragini Gunjan\nDA: Raghuveer Khare & Rashmi\nLP: Tafreeh Studios\nEditor: Abhishek Das\nPost Producer: Bhishm Wahi\nColorist: Gaurav Arora\nOnline: Santosh & Vishwas\nMotion Graphics: Ashish Singh\nAudio Mix: Yudhajit Biswas",
+        "type": "video",
+        "duration": 115,
+        "width": 1920,
+        "height": 1080,
+        "created_time": "2022-08-04T09:22:01+00:00",
+        "content_rating": [
+            "unrated"
+        ],
+        "config_url": "https://player.vimeo.com/video/736453614/config?autopause=0&autoplay=0&background=0&badge=0&byline=0&bypass_privacy=1&collections=0&context=Vimeo%5CController%5CApi%5CResources%5CUser%5CProfile%5CProfileSectionVideosController.&controls=1&default_to_hd=1&force_embed=0&h=63c8823f7a&like=1&logo=0&loop=0&muted=0&offset=0&outro=nothing&playbar=1&portrait=0&privacy_banner=0&quality=540p&responsive=1&responsive_width=1&share=1&speed=1&title=0&volume=1&watch_later=1&s=e2b8b28e6d48c98fe55e3c2d802f43f063241405_1660736999",
+        "pictures": {
+            "uri": "/videos/736453614/pictures/1481851166",
+            "active": true,
+            "type": "custom",
+            "base_link": "https://i.vimeocdn.com/video/1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d",
+            "sizes": [
+                {
+                    "width": 100,
+                    "height": 75,
+                    "link": "https://i.vimeocdn.com/video/1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_100x75?r=pad",
+                    "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_100x75&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+                },
+                {
+                    "width": 200,
+                    "height": 150,
+                    "link": "https://i.vimeocdn.com/video/1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_200x150?r=pad",
+                    "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_200x150&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+                },
+                {
+                    "width": 295,
+                    "height": 166,
+                    "link": "https://i.vimeocdn.com/video/1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_295x166?r=pad",
+                    "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_295x166&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+                },
+                {
+                    "width": 640,
+                    "height": 360,
+                    "link": "https://i.vimeocdn.com/video/1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_640x360?r=pad",
+                    "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_640x360&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+                },
+                {
+                    "width": 960,
+                    "height": 540,
+                    "link": "https://i.vimeocdn.com/video/1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_960x540?r=pad",
+                    "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_960x540&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+                },
+                {
+                    "width": 1280,
+                    "height": 720,
+                    "link": "https://i.vimeocdn.com/video/1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_1280x720?r=pad",
+                    "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_1280x720&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+                },
+                {
+                    "width": 1920,
+                    "height": 1080,
+                    "link": "https://i.vimeocdn.com/video/1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_1920x1080?r=pad",
+                    "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_1920x1080&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+                }
+            ],
+            "resource_key": "897c5245c6743af0155d7dd9d9e711cb89e341a6",
+            "default_picture": false
+        },
+        "categories": [
+            {
+                "uri": "/categories/adsandcommercials",
+                "name": "Ads and Commercials"
+            }
+        ],
+        "metadata": {
+            "connections": {
+                "comments": {
+                    "total": 0
+                }
+            }
+        },
+        "user": {
+            "uri": "/users/49086211",
+            "name": "Flying Elephant Films",
+            "link": "https://vimeo.com/flyingelephantfilms",
+            "location": "New Delhi, Delhi, India",
+            "bio": "We are a one stop shop for all things film. From conceptualising to execution, we bring our unique perspective, experience and skill set to every project. Always striving to challenge the ordinary and experimenting to break the norm, we want our every product to make us proud. An award winning team that brings its experience of film making in all formats.",
+            "membership": {
+                "badge": {
+                    "type": null
+                }
+            },
+            "pictures": {
+                "sizes": [
+                    {
+                        "width": 30,
+                        "height": 30,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_30x30"
+                    },
+                    {
+                        "width": 72,
+                        "height": 72,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_72x72"
+                    },
+                    {
+                        "width": 75,
+                        "height": 75,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_75x75"
+                    },
+                    {
+                        "width": 100,
+                        "height": 100,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_100x100"
+                    },
+                    {
+                        "width": 144,
+                        "height": 144,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_144x144"
+                    },
+                    {
+                        "width": 216,
+                        "height": 216,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_216x216"
+                    },
+                    {
+                        "width": 288,
+                        "height": 288,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_288x288"
+                    },
+                    {
+                        "width": 300,
+                        "height": 300,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_300x300"
+                    },
+                    {
+                        "width": 360,
+                        "height": 360,
+                        "link": "https://i.vimeocdn.com/portrait/11728201_360x360"
+                    }
+                ]
+            },
+            "skills": [],
+            "available_for_hire": true,
+            "background_video": [
+                {
+                    "id": 1356768542,
+                    "clip_id": "732436318",
+                    "y_position": 39,
+                    "start_time": 0,
+                    "end_time": 45,
+                    "clip": {},
+                    "first_frame_URL": "https://i.vimeocdn.com/profile_cover/1356768542",
+                    "page_url": "/732436318/d59c3876e7",
+                    "dimensions": {
+                        "width": 3840,
+                        "height": 2160
+                    },
+                    "config_url": "https://player.vimeo.com/video/732436318/config?autopause=0&autoplay=1&background=1&bypass_privacy=1&context=Vimeo%5CController%5CApi%5CResources%5CUser%5CProfile%5CProfileSectionVideosController.&default_to_hd=1&h=d59c3876e7&log_plays=0&outro=nothing&responsive=1&responsive_width=1&s=d9521fac4451f2e24a79bcaa44e1d545d713b62d_1660736999",
+                    "player_url": "player.vimeo.com",
+                    "thumbset_id": 1473951003,
+                    "aspect_ratio": 0.562429696287964,
+                    "duration": 45
+                }
+            ],
+            "is_expert": false
+        },
+        "badge": null
+    },
+    "profile_section_uri": "/users/49086211/profile_sections/328639",
+    "show_featured_comment": false,
+    "featured_comment": null,
+    "is_staffpick": false,
+    "column_width": "2"
+}]
--- utils/data/changes.js
+++ utils/data/changes.js
@@ -1,1 +1,77 @@
-[{"description":"We have solved the mystery of unwanted rashes, itching and skin infections for you. All thanks to detective Devgn!\n\n\nHere is our latest production with the one and only Ajay Devgn and Asha Negi\nClient: Piramal\nBrand: Tetmosol Soap\nAgency: BBDO India\nDirector: Siddharth Sen \nDOP: Ranga Rajan \nExecutive Producers: Sanjana Jain & Riddhindra\nCreative Supervisor: Smriti Sharma Bhaskar\nChief AD: Ragini Gunjan\nDA: Kshitij Kamble\nProduction Designer: Maddy\nStill Photographer: Jitu Salvani\nLine Production: Tafreeh Studios\nEditor: Zubin\nPost Producer: Bhishm Wahi\nOnline: Vishwas Red Flagg\nColorist: Swapnil\nMusic: Aman Pant\nAD Entourage:-\nStylist: Naveen Shetty\nHMU: Harish Wadhane & Hakim Alim","duration":30,"name":"Tetmosol Hindi Soap Film 2022 - Ajay Devgn and Asha Negi","pictures":{"uri":"/videos/728060617/pictures/1465493375","active":true,"type":"custom","base_link":"https://i.vimeocdn.com/video/1465493375-e5f52a29d99079a2e630bd9f0ebd0960d5c6a49a518cff732bd6dba7504f1118-d","sizes":[{"width":200,"height":150,"link":"https://i.vimeocdn.com/video/1465493375-e5f52a29d99079a2e630bd9f0ebd0960d5c6a49a518cff732bd6dba7504f1118-d_200x150?r=pad","link_with_play_button":"https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1465493375-e5f52a29d99079a2e630bd9f0ebd0960d5c6a49a518cff732bd6dba7504f1118-d_200x150&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"},{"width":640,"height":360,"link":"https://i.vimeocdn.com/video/1465493375-e5f52a29d99079a2e630bd9f0ebd0960d5c6a49a518cff732bd6dba7504f1118-d_640x360?r=pad","link_with_play_button":"https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1465493375-e5f52a29d99079a2e630bd9f0ebd0960d5c6a49a518cff732bd6dba7504f1118-d_640x360&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"},{"width":1280,"height":720,"link":"https://i.vimeocdn.com/video/1465493375-e5f52a29d99079a2e630bd9f0ebd0960d5c6a49a518cff732bd6dba7504f1118-d_1280x720?r=pad","link_with_play_button":"https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1465493375-e5f52a29d99079a2e630bd9f0ebd0960d5c6a49a518cff732bd6dba7504f1118-d_1280x720&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"},{"width":1920,"height":1080,"link":"https://i.vimeocdn.com/video/1465493375-e5f52a29d99079a2e630bd9f0ebd0960d5c6a49a518cff732bd6dba7504f1118-d_1920x1080?r=pad","link_with_play_button":"https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1465493375-e5f52a29d99079a2e630bd9f0ebd0960d5c6a49a518cff732bd6dba7504f1118-d_1920x1080&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"}],"resource_key":"1810a2c266359aeac4b2c60c444058bb27036a25","default_picture":false},"type":"","uri":"/videos/728060617","user":{"name":"Flying Elephant Films"},"slug":"tetmosol-hindi-soap-film-2022-ajay-devgn-and-asha-negi","id":"","title":"","blurb":"","content":"","category":[],"tags":[],"articleImage":"","thumbnailImage":"","index":"","author":[],"media":[{"id":"","mediaType":"","src":"","caption":""}],"crew":[{"id":"","name":"","photo":"","bio":"","role":""}],"awards":[{"id":"","title":"","year":"","blurb":"","reference":"","image":""}],"partners":[{"id":"","name":"","website":"","image":""}]},{"description":"The love for storytelling took us to Goa! And we’re high on this one. Presenting India’s very own homegrown Feni @cazulopremiumfeni @feni_dotor\n\nClient: HP India\nProduction House: Flying Elephant Films\nDirector: Sanjana Jain\nDP: Shakti Raj Jadeja\n\nCreative Director: Smriti Sharma Bhaskar\nDA: Himika Shrama\nProduction: Hamza Chaudhary\nLine Production: Black Panther Productions\nPost Producer: Bhishm Wahi\nEditor & Grade: Abhishek Das\nMusic: BMG Production Music","duration":60,"name":"HP X Cazulo Director's Cut","pictures":{"uri":"/videos/729964914/pictures/1469110468","active":true,"type":"custom","base_link":"https://i.vimeocdn.com/video/1469110468-f8c8b3bc13ba5b6a7b1ae52b1b7eeee4927e4af539f4f6e7a626b0dfabab6650-d","sizes":[{"width":200,"height":150,"link":"https://i.vimeocdn.com/video/1469110468-f8c8b3bc13ba5b6a7b1ae52b1b7eeee4927e4af539f4f6e7a626b0dfabab6650-d_200x150?r=pad","link_with_play_button":"https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1469110468-f8c8b3bc13ba5b6a7b1ae52b1b7eeee4927e4af539f4f6e7a626b0dfabab6650-d_200x150&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"},{"width":640,"height":360,"link":"https://i.vimeocdn.com/video/1469110468-f8c8b3bc13ba5b6a7b1ae52b1b7eeee4927e4af539f4f6e7a626b0dfabab6650-d_640x360?r=pad","link_with_play_button":"https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1469110468-f8c8b3bc13ba5b6a7b1ae52b1b7eeee4927e4af539f4f6e7a626b0dfabab6650-d_640x360&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"},{"width":1280,"height":720,"link":"https://i.vimeocdn.com/video/1469110468-f8c8b3bc13ba5b6a7b1ae52b1b7eeee4927e4af539f4f6e7a626b0dfabab6650-d_1280x720?r=pad","link_with_play_button":"https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1469110468-f8c8b3bc13ba5b6a7b1ae52b1b7eeee4927e4af539f4f6e7a626b0dfabab6650-d_1280x720&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"},{"width":1920,"height":1080,"link":"https://i.vimeocdn.com/video/1469110468-f8c8b3bc13ba5b6a7b1ae52b1b7eeee4927e4af539f4f6e7a626b0dfabab6650-d_1920x1080?r=pad","link_with_play_button":"https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1469110468-f8c8b3bc13ba5b6a7b1ae52b1b7eeee4927e4af539f4f6e7a626b0dfabab6650-d_1920x1080&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"}],"resource_key":"ac0b76c52681d42cd4313ea54f48b7e5e85f4d47","default_picture":false},"type":"","uri":"/videos/729964914","user":{"name":"Flying Elephant Films"},"slug":"hp-x-cazulo-directors-cut","id":"","title":"","blurb":"","content":"","category":[],"tags":[],"articleImage":"","thumbnailImage":"","index":"","author":[],"media":[{"id":"","mediaType":"","src":"","caption":""}],"crew":[{"id":"","name":"","photo":"","bio":"","role":""}],"awards":[{"id":"","title":"","year":"","blurb":"","reference":"","image":""}],"partners":[{"id":"","name":"","website":"","image":""}]}]
+[{
+    "description": "The refreshing national crush of India - Rohit Saraf\nEveryone’s center of attention and queen - Alaya\nThe ‘love storiyan’ that everyone is hating but this no one can hate. Presenting our latest film for Center Fresh India\n\nBrand: Center Fresh India\nCreative Agency: Ogilvy India\nMedia Agency: Wave Maker India\n\nDirector: Sanana Jain\nExecutive Producer: Riddhindra Chaudhuri\nCreative Supervisor: Smriti Sharma Bhaskar\nDP: Sombit Mondal\nAC: Roshan Jose\nChief AD: Ragini Gunjan\nDA: Raghuveer Khare & Rashmi\nLP: Tafreeh Studios\nEditor: Abhishek Das\nPost Producer: Bhishm Wahi\nColorist: Gaurav Arora\nOnline: Santosh & Vishwas\nMotion Graphics: Ashish Singh\nAudio Mix: Yudhajit Biswas",
+    "duration": 115,
+    "name": "Center Fresh - Fresh Breath Day",
+    "pictures": {
+        "uri": "/videos/736453614/pictures/1481851166",
+        "active": true,
+        "type": "custom",
+        "base_link": "https://i.vimeocdn.com/video/1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d",
+        "sizes": [{
+            "width": 200,
+            "height": 150,
+            "link": "https://i.vimeocdn.com/video/1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_200x150?r=pad",
+            "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_200x150&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+        }, {
+            "width": 640,
+            "height": 360,
+            "link": "https://i.vimeocdn.com/video/1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_640x360?r=pad",
+            "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_640x360&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+        }, {
+            "width": 1280,
+            "height": 720,
+            "link": "https://i.vimeocdn.com/video/1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_1280x720?r=pad",
+            "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_1280x720&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+        }, {
+            "width": 1920,
+            "height": 1080,
+            "link": "https://i.vimeocdn.com/video/1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_1920x1080?r=pad",
+            "link_with_play_button": "https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1481851166-d62c84b9cc06b4838ecb346cd0f578b8e9e45d0fb626ed4dd62475e510f4952f-d_1920x1080&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"
+        }],
+        "resource_key": "897c5245c6743af0155d7dd9d9e711cb89e341a6",
+        "default_picture": false
+    },
+    "type": "",
+    "uri": "/videos/736453614",
+    "user": {
+        "name": "Flying Elephant Films"
+    },
+    "slug": "center-fresh-fresh-breath-day",
+    "id": "",
+    "title": "",
+    "blurb": "",
+    "content": "",
+    "category": [],
+    "tags": [],
+    "articleImage": "",
+    "thumbnailImage": "",
+    "index": "",
+    "author": [],
+    "media": [{
+        "id": "",
+        "mediaType": "",
+        "src": "",
+        "caption": ""
+    }],
+    "crew": [{
+        "id": "",
+        "name": "",
+        "photo": "",
+        "bio": "",
+        "role": ""
+    }],
+    "awards": [{
+        "id": "",
+        "title": "",
+        "year": "",
+        "blurb": "",
+        "reference": "",
+        "image": ""
+    }],
+    "partners": [{
+        "id": "",
+        "name": "",
+        "website": "",
+        "image": ""
+    }]
+}]
